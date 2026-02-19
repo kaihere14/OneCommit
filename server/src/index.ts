dc201a15385ff292f0aedcfdca575e71467059f8
kaihere14 <yashpalsharma9950@gmail.com>
2026-02-19 20:08:08 +0530

    feat: Implement dotenv and a health endpoint for the server, configure Prettier for auto-formatting, and update gitignore.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,14 @@
+import "dotenv/config";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
+  res.send("OneCommit Server is running!");
+});
+
+app.get("/api/health", (req, res) => {
   res.send("OneCommit Server is running!");
 });
 
